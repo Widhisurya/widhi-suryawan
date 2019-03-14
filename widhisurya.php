ed23db2a5e0c69a462e9cc7b45a45c59ab160e14
Widhisurya <48555001+Widhisurya@users.noreply.github.com>
2019-03-14 19:32:24 +0700

    Add files via upload

---
+++ widhisurya.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World!</p>'; ?> 
+   <?php echo '<p>Nama: Widhi Suryawan</p>'; ?> 
+    <?php echo '<p>NIM: 14.01.53.0049</p>'; ?> 
+ </body>
+</html>
